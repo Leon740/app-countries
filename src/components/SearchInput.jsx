a1549e0c4dbd05552f897d894ffc91da74801ae7
Leon740 <dobrinovleonid@gmail.com>
2022-09-30 16:31:24 -0400

    feat(app): add SearchInput focus

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -8,7 +8,7 @@ function SearchInput({ fnOnChange }) {
       <BsSearch className="mr-6 text-xl absolute left-6 top-4" />
       <input
         placeholder="Search by name"
-        className="text-base pr-6 pl-16 pt-4 pb-4 w-full outline-0 bg-white dark:bg-customgray-200 rounded-lg shadow-md shadow-slate-200 dark:shadow-gray-800 focus:shadow-lg focus:shadow-slate-300 dark:focus:shadow-gray-900 transition ease-in-out duration-300"
+        className="text-base pr-6 pl-16 pt-4 pb-4 w-full outline-0 bg-white dark:bg-customgray-200 rounded-lg shadow-md shadow-slate-200 dark:shadow-gray-800 focus:shadow-inner focus:shadow-slate-300 dark:focus:shadow-gray-900"
         onChange={(event) => fnOnChange(event.target.value)}
       />
     </div>
