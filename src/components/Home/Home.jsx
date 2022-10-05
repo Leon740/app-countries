22712e77dfe2c829cbe21dc31d6e37954d4acf98
Leon740 <dobrinovleonid@gmail.com>
2022-10-04 20:47:39 -0400

    feat(app): filter

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-no-bind */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import fnSanitizeString from '../../utils/fnSanitizeString';
 import ALL_COUNTRIES from '../../data.json';
 import SearchControls from './SearchControls';
@@ -8,7 +8,9 @@ import DisplayError from './DisplayError';
 import CardSmall from './CardSmall';
 
 function Home() {
-  const [stCountries, setStCountries] = useState(ALL_COUNTRIES);
+  // eslint-disable-next-line no-unused-vars
+  const [stDisplayCountries, setStDisplayCountries] = useState(ALL_COUNTRIES);
+  const [stMatchingCountries, setStMatchingCountries] = useState(ALL_COUNTRIES);
 
   function fnSearchBy(type) {
     // eslint-disable-next-line func-names
@@ -16,21 +18,19 @@ function Home() {
       console.log(value);
       let result;
       switch (type) {
-        case 'name': result = ALL_COUNTRIES.filter((country) => fnSanitizeString(country.name.common).includes(fnSanitizeString(value)));
+        case 'name':
+          result = stMatchingCountries.filter((country) => fnSanitizeString(country.name.common).includes(fnSanitizeString(value)));
+          setStDisplayCountries(result);
           break;
-        case 'region': result = ALL_COUNTRIES.filter((country) => fnSanitizeString(country.region).includes(fnSanitizeString(value)));
+        case 'region':
+          result = ALL_COUNTRIES.filter((country) => fnSanitizeString(country.region).includes(fnSanitizeString(value)));
+          setStMatchingCountries(result.length > 0 ? result : ALL_COUNTRIES);
+          setStDisplayCountries(result.length > 0 ? result : ALL_COUNTRIES);
           break;
         default: result = ALL_COUNTRIES;
       }
-      console.log(result);
-      setStCountries(result || ALL_COUNTRIES);
     };
   }
-
-  useEffect(() => {
-    // console.clear();
-    // stCountries.map((country) => console.log(country.name.common));
-  }, [stCountries]);
 
   const fnSearchByName = fnSearchBy('name');
   const fnSearchByRegion = fnSearchBy('region');
@@ -40,10 +40,10 @@ function Home() {
       <Container>
         <SearchControls fnInputOnChange={fnSearchByName} fnSelectOnChange={fnSearchByRegion} />
 
-        {stCountries.length > 0
+        {stDisplayCountries.length > 0
           ? (
             <ul className="flex flex-wrap -mt-8 -ml-8 -mr-8 mb-8">
-              {stCountries.map((country) => <CardSmall key={country.name.common} country={country} />)}
+              {stDisplayCountries.map((country) => <CardSmall key={country.name.common} country={country} />)}
             </ul>
           )
           : (
