04397bf312e4af520318c65b0792db25b4e89b94
Leon740 <dobrinovleonid@gmail.com>
2022-10-19 17:54:59 -0400

    feat(app): Sidebar

--- src/components/Home/Sidebar/Sidebar.jsx
+++ src/components/Home/Sidebar/Sidebar.jsx
@@ -34,8 +34,14 @@ function Sidebar() {
   }));
 
   return (
-    <div className={`h-screen min-h-max fixed xl:sticky top-0 xl:top-20 pt-20 xl:pt-0 xl:pb-20 w-64 xl:w-full -left-64 ${stSidebar ? 'left-0' : ''} xl:left-0 transition-all duration-300 bg-white dark:bg-customgray-200 shadow-md shadow-slate-200 dark:shadow-gray-800`}>
-      <div className="p-8 h-full overflow-y-auto">
+    <div
+      className={`
+      fixed w-64 top-20  ${stSidebar ? 'left-0' : '-left-64'}
+      xl:sticky xl:w-full
+      p-8 bg-white dark:bg-customgray-200 shadow-md shadow-slate-200 dark:shadow-gray-800 transition-all duration-300`}
+      style={{ height: 'calc(100vh - 5rem)' }}
+    >
+      <div className="h-full overflow-y-auto">
         <FilterByName />
         <Accordion name="Regions" options={optionsRegions} fnSelect={fnRegionAdd} fnUnSelect={fnRegionRemove} />
         <Accordion name="Languages" options={optionsLanguages} fnSelect={fnLanguageAdd} fnUnSelect={fnLanguageRemove} />
